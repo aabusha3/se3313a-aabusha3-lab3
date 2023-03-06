e7a532d2b529c5e14a1152a085d759d31d17c505
aabusha3 <90729952+aabusha3@users.noreply.github.com>
2023-03-06 01:30:52 -0500

    define client socket handler on server

--- Server.cpp
+++ Server.cpp
@@ -4,11 +4,75 @@
 #include <time.h>
 #include <list>
 #include <vector>
+#include <algorithm>
 
 using namespace Sync;
+using namespace std;
 
+class SocketThread : public Thread
+{
+private:
+    Socket& socket;
+    
+    ByteArray data;
+
+    bool &terminate;
+
+    vector<SocketThread*> &sockThrHolder;
+
+public:
+    SocketThread(Socket& socket, bool &terminate, vector<SocketThread*> &clientSockThr)
+    : socket(socket), terminate(terminate), sockThrHolder(clientSockThr)
+    {}
+
+    ~SocketThread()
+    {this->terminationEvent.Wait();}
+
+    Socket& GetSocket()
+    {
+        return socket;
+    }
+
+    virtual long ThreadMain() {
+
+        try{
+            while(!terminate){
+                socket.Read(data);
+
+                // Perform operations on the data
+                string res = data.ToString();
+                for_each(res.begin(), res.end(), [](char & res){
+                    res = ::toupper(res);
+                });
+                
+
+                if (res=="DONE") {
+                    sockThrHolder.erase(remove(sockThrHolder.begin(), sockThrHolder.end(), this), sockThrHolder.end());
+                    terminate = true; 
+                    break;      
+                }
+                
+                
+                res.append("-received");
+                // Send it back
+                socket.Write(ByteArray(res));
+
+            }
+            
+        }catch (string &s) {
+            cout<<s<<endl;
+        }
+		
+        catch (string err)
+        {
+            cout<<err<<endl;
+        }
+        cout<<"Client Disconnected" <<endl;
+        return 0;
+    }
+};
 
 int main(void)
 {
-    std::cout << "I am a server." << std::endl;
+    cout << "I am a server." << endl;
 }

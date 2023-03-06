07c8a2e69dc539132fa89d9ff3abb8ddbe2b83b5
aabusha3 <90729952+aabusha3@users.noreply.github.com>
2023-03-06 01:41:52 -0500

    implement server functionality

--- Server.cpp
+++ Server.cpp
@@ -59,12 +59,11 @@ public:
 
             }
             
-        }catch (string &s) {
+        }catch (string &s){
             cout<<s<<endl;
         }
 		
-        catch (string err)
-        {
+        catch (string err){
             cout<<err<<endl;
         }
         cout<<"Client Disconnected" <<endl;
@@ -72,7 +71,66 @@ public:
     }
 };
 
+class ServerThread : public Thread
+{
+private:
+    SocketServer& server;
+    bool terminate = false;
+    vector<SocketThread*> sockThrHolder;
+
+public:
+    ServerThread(SocketServer& server)
+    : server(server)
+    {}
+
+    ~ServerThread(){
+        for(auto thread: sockThrHolder){
+            try{
+                Socket& toClose = thread->GetSocket();
+                toClose.Close();
+            }catch (...){}
+        } 
+        vector<SocketThread*>().swap(sockThrHolder);
+        cout<<"Closing client from server"<<endl;
+        terminate = true;
+    }
+
+    virtual long ThreadMain(){
+        while (1){
+            try{
+                Socket* newConnection = new Socket(server.Accept());
+
+                ThreadSem serverBlock(1);
+
+                Socket& socketReference = *newConnection;
+                sockThrHolder.push_back(new SocketThread(socketReference, terminate, ref(sockThrHolder)));
+            }catch (string error){
+                cout << "ERROR: " << error << endl;
+                return 1;
+            }
+			
+			catch (TerminationException terminationException){
+				cout << "Server has shut down!" << endl;
+				return terminationException;
+			}
+        }
+        return 0;
+    }
+};
+
 int main(void)
 {
     cout << "I am a server." << endl;
+    cout << "Press enter to terminate the server.";
+    cout.flush();
+	
+    SocketServer server(3000);    
+
+    ServerThread serverThread(server);
+	
+    FlexWait cinWaiter(1, stdin);
+    cinWaiter.Wait();
+    cin.get();
+    
+    server.Shutdown();
 }
